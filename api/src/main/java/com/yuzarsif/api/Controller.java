6d5ed89e5a062e4bd3c1e62c10c127a71bcaf740
Yusuf Kılıç <yuzarsifkilic@gmail.com>
2024-06-15 08:07:53 +0300

    init commit

---
+++ api/src/main/java/com/yuzarsif/api/Controller.java
@@ -0,0 +1,15 @@
+package com.yuzarsif.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping
+    public String demo() {
+        return "Demo";
+    }
+}
