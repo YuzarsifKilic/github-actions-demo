a85c541b856a584277c7adc5fab52339b307ccd1
Yusuf Kılıç <yuzarsifkilic@gmail.com>
2024-06-19 18:51:50 +0300

    changed environments types

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DemoService} from "./demo.service";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private demoService: DemoService) { }
+
+  demo() {
+    this.demoService.demo()
+      .then(response => {
+        console.log(response);
+      })
+  }
 
 }
