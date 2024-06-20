99964a6d58a01dd77cfdf9773ebf6d7bbd054bf4
Yusuf Kılıç <yuzarsifkilic@gmail.com>
2024-06-20 09:23:45 +0300

    changed environments types

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
 export const environment = {
   production: true,
-  apiUrl: window['__env'] ? window['__env'].apiUrl : ''
+  apiUrl: window.env?.API_URL || ''
 };
---
+++ frontend/src/global.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+  env: {
+    API_URL: string;
+  };
+}
